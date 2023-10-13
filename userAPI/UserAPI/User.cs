de68fc08064e52e1483b3c3ed3ccbbf620c70a59
teja <48879182+shade8@users.noreply.github.com>
2023-10-13 18:56:44 +0530

    Add files via upload

--- userAPI/UserAPI/User.cs
+++ userAPI/UserAPI/User.cs
@@ -7,6 +7,7 @@
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
         public string Place { get; set; } = string.Empty;
-
+        public string Email {  get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
     }
 }
